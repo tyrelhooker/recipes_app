d45023eb833be795063781d2585ef6363dce41e8
TJ Hooker <tyrelhooker@gmail.com>
2020-05-16 22:29:09 -0400

    Break Recipe component down into two additional components to handle the logic of mapping each list and rendering the ingredients in each list

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,15 +1,12 @@
 import React from 'react';
+import IngredientList from './IngredientList';
+// import Ingredient from './Ingredient';
 import Instructions from './Instructions';
 
 const Recipe = ({ name, ingredients, steps }) =>
   <section id={name.toLowerCase().replace(/ /g, '-')}>
     <h1>{name}</h1>
-    <ul className='ingredients'>
-      {ingredients.map((ingredient, i) =>
-        <li key={i}>{ingredient.name}</li>
-      )}
-    </ul>
-
+    <IngredientList list={ingredients} />
     <Instructions title='Cooking Instructions' steps={steps} />
   </section>
 
