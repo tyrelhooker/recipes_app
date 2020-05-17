838e4b2adefa1609a9d8b8ebb29a5a189f90f462
TJ Hooker <tyrelhooker@gmail.com>
2020-05-16 22:02:19 -0400

    Create instructions component to break apart the current recipe component logic and rendering

---
+++ src/components/Instructions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Instructions = ({title, steps}) =>
+  <section className='instructions'>
+    <h2>{title}</h2>
+    {steps.map((step, i) => 
+      <p key={i}>{step}</p>
+    )}
+  </section>
+
+export default Instructions;
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Instructions from './Instructions';
 
 const Recipe = ({ name, ingredients, steps }) =>
   <section id={name.toLowerCase().replace(/ /g, '-')}>
@@ -8,12 +9,8 @@ const Recipe = ({ name, ingredients, steps }) =>
         <li key={i}>{ingredient.name}</li>
       )}
     </ul>
-    <section className='instructions'>
-      <h2>Cooking Instructions</h2>
-      {steps.map((step, i) => 
-        <p key={i}>{step}</p>
-      )}
-    </section>
+
+    <Instructions title='Cooking Instructions' steps={steps} />
   </section>
 
 export default Recipe;
